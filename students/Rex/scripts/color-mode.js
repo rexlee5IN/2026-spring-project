9d4375aa6231e1db0515a895f2646cf215eafe4e
Rex <rexlee.5in@gmail.com>
2026-05-07 15:53:02 +0800

    template

---
+++ students/Rex/scripts/color-mode.js
@@ -0,0 +1,14 @@
+let button = document.getElementById("color-mode-button");
+function toggleColorMode(event) {
+    console.log(document.body.classList.contains("dark-mode"));
+    if (document.body.classList.contains("dark-mode")) {
+        button.textContent = "Dark mode";
+        document.body.classList.replace("dark-mode", "light-mode");
+    } else {
+        button.textContent = "Light mode";
+        document.body.classList.replace("light-mode", "dark-mode");
+    };
+    console.log("event =", event);
+};
+// Run our function when the button is clicked.
+button.addEventListener("click", toggleColorMode);
